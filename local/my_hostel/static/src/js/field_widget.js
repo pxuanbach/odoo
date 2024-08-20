1a3ac9b6795265288d686efef596fd07050287a8
Bach Pham <pxuanbach1412@gmail.com>
2024-08-20 14:23:16 +0700

    Making RPC calls to the server

--- local/my_hostel/static/src/js/field_widget.js
+++ local/my_hostel/static/src/js/field_widget.js
@@ -1,24 +1,47 @@
 /** @odoo-module */
-import { Component} from "@odoo/owl";
+import { Component, onWillStart , onWillUpdateProps} from "@odoo/owl";
 import { renderToElement } from "@web/core/utils/render";
 import { registry } from "@web/core/registry";
 
 export class CategColorField extends Component {
   setup() {
     this.totalColors = [1,2,3,4,5,6];
+    onWillStart(() => {
+      this.loadCategInformation();
+    });
+    onWillUpdateProps(() => {
+      this.loadCategInformation();
+    });
     super.setup();
- }
- clickPill(value) {
-   this.props.record.update({ [this.props.name]: value });
- }
- categInfo(ev){
-  var $target = $(ev.target);
-  var data = $target.data();
-  $target.parent().find(".categInformationPanel").html($(renderToElement("CategInformation",{
-      value: data.value,
-      'widget': this
-  })));
-}
+  }
+  clickPill(value) {
+    this.props.record.update({ [this.props.name]: value });
+  }
+  categInfo(ev){
+    var $target = $(ev.target);
+    var data = $target.data();
+    $target.parent().find(".categInformationPanel").html($(renderToElement("CategInformation",{
+        value: data.value,
+        'widget': this
+    })));
+  }
+  async loadCategInformation() {
+    var self = this;
+    self.categoryInfo = {};
+    var resModel = self.env.model.root.resModel;
+    var domain = [];
+    var fields = ['category'];
+    var groupby = ['category'];
+    const categInfoPromise = await self.env.services.orm.readGroup(
+        resModel,
+        domain,
+        fields,
+        groupby
+    );
+    categInfoPromise.map((info) => {
+        self.categoryInfo[info.category] = info.category_count;
+    });
+  }
 }
 
 CategColorField.template = "CategColorField";
